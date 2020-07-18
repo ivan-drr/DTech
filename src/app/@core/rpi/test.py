48168f9826fd43221aeb9bed5c682c272160632d
ivan-drr <ivan.delrr@gmail.com>
2020-07-18 16:56:23 +0200

    rpi script

---
+++ src/app/@core/rpi/test.py
@@ -0,0 +1,13 @@
+import pymongo
+import RPi.GPIO as gpio
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["dtech"]
+mycol = mydb["room"]
+
+room = mycol.find_one({"name": "salon"})
+
+if room:
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(12, gpio.OUT)
+    gpio.output(12, room.state)
